1b9c197b593810e6b3fe1505d1bd50bb90e9e7a4
UbuntuMeta <fightforphp@gmail.com>
2024-02-17 23:40:31 +0800

    print the diagram of the linear regression.

--- least_squares/least_squares_method.py
+++ least_squares/least_squares_method.py
@@ -2,6 +2,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
+import numpy as np
 
 fig, ax = plt.subplots(1, 1)
 
@@ -15,3 +16,12 @@ reg = LinearRegression()
 reg.fit(X_train, y_train)
 print("coefficient vector of the model", reg.coef_)
 print("Coefficient intercept of the model", reg.intercept_)
+
+ax.scatter(X_train[:, 0], y_train, marker='o', c="g")
+ax.scatter(X_test[:, 0], y_test, marker='*', c="r")
+
+# calculate the x and y-axis
+reg_x = np.array([-3, 3])
+reg_y = reg.coef_ * reg_x + reg.intercept_
+ax.plot(reg_x, reg_y)
+plt.show()
